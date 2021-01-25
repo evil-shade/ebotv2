f73f79282c597e9d52ff02bc1b88a4881d14d515
shan <hakshaz.net@gmail.com>
2021-01-25 16:51:19 -0500

    update to run every day!

--- selebot.py
+++ selebot.py
@@ -127,13 +127,13 @@ if __name__ == '__main__':
     loop()
     # schedule.every(5).seconds.do(loop)
     # schedule.every().hour.do(job)
-    # schedule.every().day.at("10:30").do(loop)
+    schedule.every().day.at("24:00").do(loop)
     # schedule.every(5).to(10).minutes.do(job)
     # schedule.every().monday.do(job)
     # schedule.every().wednesday.at("13:15").do(job)
     # schedule.every().minute.at(":17").do(job)
 
-    # while True:
-    #     print('Waiting for right time')
-    #     schedule.run_pending()
-    #     time.sleep(1)
+    while True:
+        print('Waiting for right time')
+        schedule.run_pending()
+        time.sleep(1)
